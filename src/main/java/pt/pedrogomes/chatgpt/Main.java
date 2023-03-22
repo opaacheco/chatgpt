dd54ae1cc3f234a487cee09b07a29bbcdf64107d
Pedro Gomes <aformigamorreupedro@gmail.com>
2023-03-22 15:02:19 +0000

    first commit

---
+++ src/main/java/pt/pedrogomes/chatgpt/Main.java
@@ -0,0 +1,10 @@
+package pt.pedrogomes.chatgpt;
+
+public class Main {
+	private static final String token = "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";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
