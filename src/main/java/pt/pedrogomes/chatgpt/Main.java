2068e61415ea9815185ebe028a0f1d6e578b8e7e
Pedro Gomes <aformigamorreupedro@gmail.com>
2023-03-29 15:51:43 +0100

    configparse

--- src/main/java/pt/pedrogomes/chatgpt/Main.java
+++ src/main/java/pt/pedrogomes/chatgpt/Main.java
@@ -1,17 +1,21 @@
 package pt.pedrogomes.chatgpt;
 import pt.pedrogomes.chatgtp.model.request.*;
 import pt.pedrogomes.chatgpt.model.request.OpenAiRequest;
+import pt.pedrogomes.chatgpt.util.ConfigParser;
 import pt.pedrogomes.chatgtp.model.*;
 
+import java.io.IOException;
+import java.util.Map;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 public class Main {
-
-	public static void main(String[] args) {
-		
+	
+	public static void main(String[] args) throws IOException {
+		Map<String, String> config = ConfigParser.parse("aplication.propeties");
+		String greeting = config.get(greeting);
 		// TODO Auto-generated method stub
 		OpenAiRequest req = new OpenAiRequest();
 		req.setModel("text-davinci-003");
