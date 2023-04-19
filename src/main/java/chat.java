057324c62dad242b2d4ca6d9183c963984a46769
Pedro Gomes <aformigamorreupedro@gmail.com>
2023-04-19 14:13:28 +0100

    improve_chat_class

--- src/main/java/chat.java
+++ src/main/java/chat.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import pt.pedrogomes.chatgpt.Message;
 
 public class chat {
+    private final String prompt;
+    private final List<Message> messages;
 
+    public chat(String prompt) {
+		super();
+		this.prompt = prompt;
+		this.messages = new ArrayList<>();
+	}
+    
+	public String getPrompt() {
+        return prompt;
+    }
+	
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void addMessage(Message message) {
+    
+        this.messages.add(message);
+    }
 }
