4198ff1a37367fee28e5cf893a8819d5465d2e68
Pedro Gomes <aformigamorreupedro@gmail.com>
2023-04-19 13:44:29 +0100

    read files final

--- src/test/java/pt/pedrogomes/chatgpt/util/ConfigParserTest.java
+++ src/test/java/pt/pedrogomes/chatgpt/util/ConfigParserTest.java
@@ -2,6 +2,7 @@ package pt.pedrogomes.chatgpt.util;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -14,51 +15,59 @@ class ConfigParserTest {
 	@Test
 	void test() throws IOException {
 		Map<String, String> config = ConfigParser.parse(getTestResourcesConfigFilePath("s1mple_config.properties"));
-		
-		
+
 		assertNotNull(config, "config cannot be null");
 		assertEquals("Batatas", config.get("tolen"));
 		assertEquals("ChatGPTclient", config.get("applicationName"));
 		assertEquals(2, config.size());
 		assertNull(config.get("Batatas"), "non wxisting config value should not have a map bla bla");
 	}
+
 	@Test
-	void testParseEmptyValue() throws IOException{
+	void testParseEmptyValue() throws IOException {
 		Map<String, String> config = ConfigParser.parse(getTestResourcesConfigFilePath("empty_values.properties"));
 		assertNotNull(config, "config cannot be null");
 		assertEquals("", config.get("token"));
 		assertEquals("ChatGPTClient", config.get("applicationName"));
 		assertEquals(2, config.size());
 	}
+
 	@Test
-	void testNoSeparator() throws IOException{
+	void testNoSeparator() throws IOException {
 		Map<String, String> config = ConfigParser.parse(getTestResourcesConfigFilePath("no_separator.properties"));
 		assertNotNull(config, "config cannot be null");
 		assertEquals("HelloWorld!", config.get("token"));
 		assertEquals("pacheco", config.get("username"));
 		assertNull(config.get("applicationName"));
 		assertEquals(2, config.size());
-		}
+	}
+
 	@Test
 	void testCommentedAndEmpty() throws IOException {
 		Map<String, String> config = ConfigParser.parse(getTestResourcesConfigFilePath("no_separator.properties"));
 		assertNotNull(config, "config cannot be null");
 		assertEquals("HelloWorld!", config.get("token"));
 		assertEquals("pacheco@gmail.com", config.get("emailAddress"));
-		assertEquals("ChatGPTClient",config.get("applicationName"));
-		assertEquals(3, config.size());	
-		
+		assertEquals("ChatGPTClient", config.get("applicationName"));
+		assertEquals(3, config.size());
+
 	}
+
 	@Test
-	void testEmptyFile()  throws IOException {
+	void testEmptyFile() throws IOException {
 		Map<String, String> config = ConfigParser.parse(getTestResourcesConfigFilePath("empty.properties"));
 		assertNotNull(config, "config cannot be null");
-		assertEquals(0, config.size());	
+		assertEquals(0, config.size());
 	}
-	
+
+	@Test
+	void testFileNotFound() {
+		assertThrows(null,()->)
+	}
+
 	private String getTestResourcesConfigFilePath(String filename) {
-		Path resourcesDirectory = Paths.get("src","test","resources", filename);
+		Path resourcesDirectory = Paths.get("src", "test", "resources", filename);
 		return resourcesDirectory.toFile().getAbsolutePath();
-		}
+	}
 
 }
